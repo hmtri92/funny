39af1eed27b00c485965f66f6243c82df5f31140
hmtri92 <trihm@itsj-group.com>
2022-03-21 18:06:49 +0700

    10

---
+++ src/index.mjs
@@ -0,0 +1,28 @@
+import express from 'express';
+import morgan from 'morgan';
+import { join } from 'path';
+import { engine } from 'express-handlebars';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const port = 4000;
+
+const app = express();
+
+// Template engine
+app.engine('.hbs', engine({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+app.set('views', join(__dirname, 'resources/views'));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Static files
+app.use(express.static(join(__dirname, 'public')));
+
+app.get('/home', (req, res) => res.render('home'));
+
+app.listen(port, () => console.log(`Funny app listening on port http://localhost:${port}`));
