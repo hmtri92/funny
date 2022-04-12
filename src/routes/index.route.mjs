7a7bc01fd83a14a8e71a7d0ab2b5650760441ce9
hmtri92@gmail.com <hmtri92@gmail.com>
2022-04-12 17:30:20 +0700

    test

--- src/routes/index.route.mjs
+++ src/routes/index.route.mjs
@@ -1,7 +1,7 @@
 import newsRouter from './news.route.mjs';
 import site from './site.route.mjs';
 
-function route(app) {
+      function route(app) {
       app.get('/news', newsRouter);
 
   app.get('/', site);
